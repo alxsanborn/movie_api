902670b7ac68d47ac2d786c1f4e1dc512cf50cb0
jasmin47 <alxsanborn@gmail.com>
2016-09-01 17:41:40 -0400

    move category titles to js page

--- app.js
+++ app.js
@@ -3,29 +3,31 @@ console.log("App.js is running");
 
 $('button#get_movie').on('click', function() {
 
-    var baseUrl = 'http://www.omdbapi.com/?t=';
+    var baseUrl = 'http://www.omdbapi.com/?';
     var movieName = $('input#movie_input').val();
     $.ajax({
-         url: baseUrl + movieName,
+         url: baseUrl + "t=" + movieName,
          method: 'GET',
          dataType: 'json',
          success: function(response, status) {
-            var imageSRC = response.Poster;
-            var title = response.Title;
-            var year = response.Year;
-            var rating = response.Rated;
-            var plot = response.Plot;
-            // debugger
-        $('img#moviePosterDefault').attr('src', imageSRC);
-        $('h4#movieTitle').text(title);
-        $('h4#movieYear').text(year);
-        $('h4#movieRating').text(rating);
-        $('h4#moviePlot').text(plot);
 
 
+             var imageSRC = response.Poster;
+             var title = response.Title;
+             var year = response.Year;
+             var rating = response.Rated;
+             var plot = response.Plot;
 
 
-       }
+            $('img#moviePosterDefault').attr('src', imageSRC);
+
+             $('h4#movieTitle').text(`Movie title: ${title}`);
+             $('h4#movieYear').text(`Year: ${year}`);
+             $('h4#movieRating').text(`Rating: ${rating}`);
+             $('h4#moviePlot').text(`Plot: ${plot}`);
+
+
+           }
     });
 $("input").val("");
 });
